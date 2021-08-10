500b77651bc3ebd0b50095b4f586e9f2a7428123
Darcy Chen <mrdarcychen@gmail.com>
2021-08-10 19:12:27 +0800

    Map config directory to XDG_CONFIG_HOME

---
+++ config/nvim/init.vim
@@ -0,0 +1,2 @@
+source $XDG_CONFIG_HOME/nvim/plugin/autocmds.vim
+source $XDG_CONFIG_HOME/nvim/plugin/settings.vim
--- configs/sway/scripts/toggle-ibus-engine.sh
+++
@@ -1,7 +0,0 @@
-current_engine=$(ibus engine)
-if [ "${current_engine}" == "xkb:us::eng" ]
-then
-  ibus engine rime
-else
-  ibus engine xkb:us::eng
-fi
