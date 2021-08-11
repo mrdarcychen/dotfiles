1a8e88f9992227d6f7dc107abcaca7bd4c7a89b4
Darcy Chen <mrdarcychen@gmail.com>
2021-08-11 14:00:03 +0800

    Add Z shell starting and shell-completion scripts
    The current strategy is to setup symlinks for .zshenv and .zshrc files.

---
+++ config/zsh/environment.zsh
@@ -0,0 +1,9 @@
+# Used for setting up shell environment (.zshenv)
+
+# Specify XDG base directories
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+ZDOTDIR=$XDG_DATA_HOME/zsh
+
