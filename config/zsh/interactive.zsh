1ae8b88b851fd6d013a2f1638f2dd5ce489879e1
Darcy Chen <mrdarcychen@gmail.com>
2021-10-22 19:24:10 +0800

    Add configurations for GnuPG

--- config/zsh/interactive.zsh
+++ config/zsh/interactive.zsh
@@ -4,6 +4,10 @@ HISTFILE=$XDG_DATA_HOME/zsh/.zsh_history
 HISTSIZE=1000
 SAVEHIST=1000
 
+# Use gpg-agent in place of ssh-agent
+export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+gpgconf --launch gpg-agent
+
 # Options for changing directories
 setopt AUTO_CD PUSHD_IGNORE_DUPS PUSHD_MINUS PUSHD_SILENT
 
